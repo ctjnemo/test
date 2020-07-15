d2ca43df632390e98bcdad4dee3c5ca14c4c7570
Tianjiao Chen <ctjnemo@gmail.com>
2020-07-15 14:29:44 -0400

    version Tianjiao

---
+++ src/jenkinsngrok/JenkinsNgrok.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jenkinsngrok;
+
+/**
+ *
+ * @author 15145
+ */
+public class JenkinsNgrok {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        System.out.println("Bonjour "+args[1]+", " +args[0]);
+    }
+    
+}
